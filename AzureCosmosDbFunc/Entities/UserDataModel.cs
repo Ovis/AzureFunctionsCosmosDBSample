48deb1fd96ce9271563bc4cb55e18d3291f4b2e8
Ovis <me+github@hitsujin.jp>
2020-04-06 00:51:44 +0900

    不要項目の削除

--- AzureCosmosDbFunc/Entities/UserDataModel.cs
+++ AzureCosmosDbFunc/Entities/UserDataModel.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
-
-namespace AzureCosmosDbFunc.Entities
+﻿namespace AzureCosmosDbFunc.Entities
 {
     public class UserDataModel
     {
-        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
         // やること
-        [JsonProperty(PropertyName = "fullName")]
         public Name Name { get; set; }
 
-        [JsonProperty(PropertyName = "age")]
         public int Age { get; set; }
-
-        [JsonProperty(PropertyName = "personalId")]
-        public string PersonalId { get; set; } = Guid.NewGuid().ToString();
 
         public string PartitionKey { get; set; } = "UserData";
     }
 
     public class Name
     {
-        [JsonPropertyName("givenName")]
         public string GivenName { get; set; }
 
-        [JsonPropertyName("familyName")]
         public string FamilyName { get; set; }
 
         public string FullName => FamilyName + GivenName;
